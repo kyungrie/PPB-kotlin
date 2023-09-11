7f8c0ba695c0358ca8be11ec59534345c321743c
Arie Ginanjar Januar <59875697+kyungrie@users.noreply.github.com>
2023-09-11 12:31:01 +0700

    Add files via upload

---
+++ PPB_Arie_Ginanjar_Januar_XI_RPL-03/BatchFile/BatchFile.kt
@@ -0,0 +1,8 @@
+fun main() {
+    var nama:String="Arie Gj"
+    println("----------------------------------------------------")
+    println("Hello, Nama saya " + nama)
+
+    println("\n...Program ini dijalankan melalui batch file")
+    println("----------------------------------------------------")
+}
