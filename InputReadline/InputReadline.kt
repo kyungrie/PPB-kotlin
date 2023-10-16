bc1b3b2bcefc2819250181607fcde1a05a7f2a60
ARIE\Lenovo <kashikiku.kyungrie12@gmail.com>
2023-10-16 11:13:39 +0700

    update

--- InputReadline/InputReadline.kt
+++ InputReadline/InputReadline.kt
@@ -2,6 +2,6 @@ fun main(){
     println("------------------------------------")
     println("Latihan Input Data Bebas : Arie Gj")
     println("------------------------------------")
-    println("Ketikkan sesuatu, lalu tekan enter untukl mengakhiri program) : ")
+    println("Ketikkan sesuatu, lalu tekan enter untuk mengakhiri program) : ")
     readLine()
 }
